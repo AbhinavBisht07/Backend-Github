ed400c4324fc4437e9c4f8531c2930d8db17e9a4
AbhinavBisht07 <bishtabhinav0077@gmail.com>
2026-04-04 16:56:42 +0530

    feat: Initialize backend for AI Battle Arena
    - Add package.json with dependencies and scripts for development
    - Create server.ts to set up the Express server
    - Implement graph.ai.ts for handling AI model interactions and state management
    - Add models.ai.ts to define AI models using LangChain
    - Set up app.ts for routing and handling requests
    - Create config.ts for environment variable management
    - Add tsconfig.json for TypeScript configuration

---
+++ ai-battle-arena/Backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import runGraph from "./ai/graph.ai.js";   
+
+const app = express();
+
+
+app.get("/heath", (req,res)=>{
+    res.send("Hello, world");
+})
+
+app.get("/", async(req,res)=>{
+    const result = await runGraph("Write a code for factorial function in js.")
+
+    res.json(result)
+})
+
+
+export default app
