b9e301b47d0e17d198f5916e0ad6465ce878085e
AbhinavBisht07 <bishtabhinav0077@gmail.com>
2026-04-09 21:37:58 +0530

    feat: add routing with react-router and create initial app routes

--- snitch/Frontend/src/app/App.jsx
+++ snitch/Frontend/src/app/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={routes} />
   )
 }
 
---
+++ snitch/Frontend/src/app/app.routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Welcome to Snitch </h1>
+    }
+])
